da2761da5737a935ffb8ca52fedafe2d6057315f
meed ghoufrane <baloncesmeed@gmail.com>
2023-09-19 11:31:42 +0100

    same exr in tables

---
+++ les tableau/1/main.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	// some et p de table
+//	int T[4],V[4],i;
+//    float P,s;
+//   
+//   for(i=0;i<4;i++){
+//   	printf("enter value of T[%d]: ",i+1);
+//   	scanf("%d",&T[i]);
+//   	printf("enter value of V[%d]: ",i+1);
+//   	scanf("%d",&V[i]);
+//   }
+//   for(i=0;i<4;i++){
+//   	//s = s + T[i];
+//   	P = P + V[i]*T[i];
+//   }
+//   printf("la rest est : %f",P);
+
+ //==========================================
+ 
+ //find max or min :
+ 
+ 
+
+ /*int T[10];
+  int min,i;
+    for(i=0;i<10;i++){
+  	    printf("enter the values T[%d] :",i+1);
+     	scanf("%d",&T[i]);
+	}
+	
+	min=T[0];
+	for(i=1;i<10;i++){
+		if(min>T[i])
+			min=T[i];
+	}
+      printf("le min est :%d",min);*/
+   /*  
+   
+   
+   
+   
+   //================================================
+   // find  element in table
+   
+    int T[10];
+    int i,n;
+    for(i=0;i<10;i++){
+  	    printf("enter the values T[%d] :",i+1);
+     	scanf("%d",&T[i]);
+	}
+    printf("enter ur number :");
+    scanf("%d",&n);
+    
+    int x=0;
+    for(i=0;i<10;i++){
+    	if(T[i]==n){
+    		x++;
+		}
+	}
+	if(x==0){
+		printf("not found !!");
+	}else{
+		printf("%d in list !!",n);
+
+	}*/
+	
+
+	return 0;
+}
